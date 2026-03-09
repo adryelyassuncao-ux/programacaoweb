6bf2bde614eb7cef11df0a6201c1554869660786
adryelyassuncao-ux <assuncaoadryely@gmail.com>
2026-03-09 11:40:59 +0000

    final

--- src/index.php
+++ src/index.php
@@ -6,15 +6,38 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-  $pessoa = [
-    'cpf'=> 123123123,
-    'nome' => "amanda siolva",
-    'idade' => 30,
-    'sexo' => 'F'
-    
+  <!-- <div class="container">
+    <fieldset>
+      <legend>Dados do funcionário</legend>
+      <form method="get">
+        <label> for="input_nome">Nome<</label>
+        <input type="text" name="txtNome" id="input_nome">
+        <input type="submit" value="Enviar">
+      </form>
+    </fieldset>
+      
+  </div> -->
 
-  ]
+  <form method="get" action="tabuada.php">
+    
+  </form>
+
+    <?php
+    // $nome = $_GET['txtNome'] ?? "sem nome";
+    // echo "<h1>O nome do funcionário é: $nome</h1>";
+
+
+  // $pessoa = [
+  //   'cpf'=> 123123123,
+  //   'nome' => "amanda siolva",
+  //   'idade' => 30,
+  //   'sexo' => 'F',
+  //   'salario' => 2000,
+  //   'habilitado' => true,
+  //   'casado' => false,
+  // ]
+
+   
     ?>
 </body>
 </html>
