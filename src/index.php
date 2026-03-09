76d958f0851762004a187842b8cbab03755ed655
adryelyassuncao-ux <assuncaoadryely@gmail.com>
2026-03-09 10:17:36 +0000

    final aula_01

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+  $pessoa = [
+    'cpf'=> 123123123,
+    'nome' => "amanda siolva",
+    'idade' => 30,
+    'sexo' => 'F'
+    
+
+  ]
+    ?>
+</body>
+</html>
