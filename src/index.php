5c5d8ddd147eb1bf811d17c04368c5ff7477a8e3
adryelyassuncao-ux <assuncaoadryely@gmail.com>
2026-04-06 10:42:19 +0000

    final aula_05

--- src/index.php
+++ src/index.php
@@ -1,69 +1,54 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .container{
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+        .imagens{
+                display: flex;
+                flex-direction: row;
+                flex-wrap: wrap;
+                justify-content: center;
+        }
+        figure{
+            width: 300px;
+            background-color: #d6d4d4;
+            border: solid 1px #1a1919;
+            border-radius: 5px;
+            display: flex;
+            flex-direction: column;
+            img{
+                margin: 15px auto;
+                width: 90%;
+                border-radius: 10px;
+            }
+            figcaption{
+                text-align: center;
+                font-size: 34px;
+            }
+        }
+    </style>
 </head>
 <body>
-    <!-- <div class="container">
-        <fieldset>
-            <legend>Dados do funcioário</legend>
-            <form method="get">
-                <label for ="input_nome">Nome:</label>
-                <input type="text" name="txtNome" id="input_nome">
-                <input type="submit" value ="Enviar">
-            </form>
-        </fieldset>
-    </div>  -->
-
-  <fieldset style="max-width: 50%; margin: 0 auto;">
-      <legent>Tabuada dinâmica</legend>
-      <form method="get">
-        <label for="">Início</label>
-        <input type="number" name="txt_inicio">
-        <br>
-        <label for="">Fim</label>
-        <input type="number" name="txt_fim">
-        <br>
-        <label for="">Tabuada</label>
-        <input type="number" name="txt_tab">
-        <br>
-        <input type="submit" name="btn_calc" value="Calcular">
-    </form>
-</fieldset>
-    
-    <?php
-    
-     
-
-        if(isset($_GET['btn_calc'])){
-            $inicio = $_GET['txt_inicio'];
-            $fim = $_GET['txt_fim'];
-            $tab = $_GET['txt_tab'];
-
-            for($i = $inicio; $i <= $fim; $i++){
-                $result = $i * $tab;
-                echo "<h3>{$i} x {$tab} = {$result}</h3>";
-            }
-
-            echo '<h1>Utilizando o while </h1>';
-            while($inicio <= $fim){
-            $result = $inicio * $tab;
-            echo "<h3> {$inicio} x {$tab} = {$result} </h3>\n";
-            $inicio++;
-            }
-            echo '<h1>Utilizando "do while" </h1>';
-            do{
-              $result = $inicio * $tab;
-              echo "<h3>{$inicio} x {stab} = {$result}</h3>\n";
-              $inicio++;
-
-            }while($inicio < $fim);
-        }
-
-           
-    
-    ?>
+    <div class="container">
+        <div class="imagens">
+            <?php
+                include 'dados_imagens.php'; //require 
+                foreach ($imagens as $key => $value) {
+                    echo "
+                        <figure>
+                            <img src='{$value}'>
+                            <figcaption>{$key}</figcaption>
+                        </figure>
+                    ";
+                }
+            ?>
+        </div>
+    </div>
 </body>
 </html>
