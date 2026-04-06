4dd136265e93ce4531c6bbb2194c8da49865436e
adryelyassuncao-ux <assuncaoadryely@gmail.com>
2026-04-06 11:40:07 +0000

    atividade

--- src/index.php
+++ src/index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Document</title>
     <style>
         .container{
@@ -35,17 +36,32 @@
     </style>
 </head>
 <body>
+<div class="card" style="width: 18rem;">
+  <img src="https://imgcentauro-a.akamaihd.net/900x900/88268831/top-fitness-nike-victory-shape-h-s-adulto-img.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+
     <div class="container">
         <div class="imagens">
             <?php
                 include 'dados_imagens.php'; //require 
-                foreach ($imagens as $key => $value) {
-                    echo "
-                        <figure>
-                            <img src='{$value}'>
-                            <figcaption>{$key}</figcaption>
-                        </figure>
-                    ";
+                foreach ($produtos as $item) {
+                    echo 
+                        <div class="card" style="width: 18rem;">
+                        <img src="https://imgcentauro-a.akamaihd.net/900x900/88268831/top-fitness-nike-victory-shape-h-s-adulto-img.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+</div>
+
+                    
                 }
             ?>
         </div>
